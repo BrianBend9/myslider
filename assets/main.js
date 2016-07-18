1843ccabf68dfaff7e0412bd5d62c1bef28cba6d
Sam <contactsamie@gmail.com>
2016-07-17 23:18:12 -0400

    Separate methods into public and private ones
    This allows for some control over what methods the client can have
    access to as opposed to other methods that are only used internally

--- assets/main.js
+++ assets/main.js
@@ -1,115 +1,129 @@
-var MySlider = (function() {
+var MySlider = (function () {
 
-  'use strict'
+	'use strict'
 
-  var imageIndex = 0;
-  var sliderElementId;
-  var timeoutId;
+	var imageIndex = 0;
+	var sliderElementId;
+	var timeoutId;
 
-  return {
-    config: {
-      "navButtons": true,
-      "autoPlay": false,
-      "autoPlayDelay": 5000
-    },
+	var config = {
+		"navButtons" : true,
+		"autoPlay" : false,
+		"autoPlayDelay" : 5000
+	};
 
-    init: function(elementId, configObject) {
-      var sliderElement = document.getElementById(elementId);
+	var init = function (elementId, configObject)  {
+		var sliderElement = document.getElementById(elementId);
 
-      sliderElementId = elementId;
-      this.addWrapper(sliderElement);
-      this.updateConfig(configObject);
-      sliderElement.classList.add('myslider');
-      sliderElement.children[imageIndex].classList.add("active");
+		sliderElementId = elementId;
+		this.addWrapper(sliderElement);
+		this.updateConfig(configObject);
+		sliderElement.classList.add('myslider');
+		sliderElement.children[imageIndex].classList.add("active");
 
-      if (this.config.navButtons === true) {
-        this.addNavButton("▶", "previous-button", sliderElement);
-        this.addNavButton("◀", "next-button", sliderElement);
-        this.addButtonEventListeners();
-      }
+		if (this.config.navButtons === true) {
+			this.addNavButton("▶", "previous-button", sliderElement);
+			this.addNavButton("◀", "next-button", sliderElement);
+			this.addButtonEventListeners();
+		}
 
-      if (this.config.autoPlay === true) {
-        this.startAutoPlayTimer(this.slide, this.config.autoPlayDelay);
-      }
-    },
+		if (this.config.autoPlay === true) {
+			this.startAutoPlayTimer(this.slide, this.config.autoPlayDelay);
+		}
+	};
 
-    updateConfig: function(newConfigObject) {
-      if ( typeof(newConfigObject) === "object") {
-        for (var setting in newConfigObject) {
-          this.config[setting] = newConfigObject[setting];
-        }
-      }
-    },
+	var updateConfig = function (newConfigObject) {
+		if (typeof(newConfigObject) === "object") {
+			for (var setting in newConfigObject) {
+				this.config[setting] = newConfigObject[setting];
+			}
+		}
+	};
 
-    addWrapper: function(sliderElement) {
-      var mysliderParent = sliderElement.parentNode;
-      var wrapper = document.createElement('div');
-      wrapper.classList.add('myslider-wrapper');
-      mysliderParent.replaceChild(wrapper, sliderElement);
-      wrapper.appendChild(sliderElement);
-    },
+	var addWrapper = function (sliderElement) {
+		var mysliderParent = sliderElement.parentNode;
+		var wrapper = document.createElement('div');
+		wrapper.classList.add('myslider-wrapper');
+		mysliderParent.replaceChild(wrapper, sliderElement);
+		wrapper.appendChild(sliderElement);
+	};
 
-    addNavButton: function(buttonText, buttonClass, sliderElement) {
-      var button = document.createElement("button");
-      var text = document.createTextNode(buttonText);
-      button.appendChild(text);
-      button.classList.add("nav-button", buttonClass);
-      sliderElement.parentNode.insertBefore(button, sliderElement);
-    },
+	var addNavButton = function (buttonText, buttonClass, sliderElement) {
+		var button = document.createElement("button");
+		var text = document.createTextNode(buttonText);
+		button.appendChild(text);
+		button.classList.add("nav-button", buttonClass);
+		sliderElement.parentNode.insertBefore(button, sliderElement);
+	};
 
-    addButtonEventListeners: function() {
-      var buttons = document.getElementsByClassName('nav-button');
-      for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", this.slide, false);
-      }
-    },
+	var addButtonEventListeners = function () {
+		var buttons = document.getElementsByClassName('nav-button');
+		for (var i = 0; i < buttons.length; i++) {
+			buttons[i].addEventListener("click", this.slide, false);
+		}
+	};
 
-    slide: function(event) {
-      var images = document.getElementById(sliderElementId).children;
+	var slide = function (event) {
+		var images = document.getElementById(sliderElementId).children;
 
-      if (event !== undefined && timeoutId !== undefined) {
-        MySlider.resetAutoPlayTimer();
-      }
+		if (event !== undefined && timeoutId !== undefined) {
+			MySlider.resetAutoPlayTimer();
+		}
 
-      if ((event === undefined || event.target.classList.contains("next-button")) && imageIndex === images.length - 1) {
-        MySlider.resetAllImagePositions(images);
-        imageIndex = 0;
-        images[imageIndex].classList.add("active");
-      } else if ((event === undefined || event.target.classList.contains("next-button")) && imageIndex < images.length - 1) {
-        MySlider.slideImagesToNextPosition(images);
-      } else if (event.target.classList.contains("previous-button") && imageIndex > 0) {
-        MySlider.slideImagesToPreviousPosition(images);
-      }
-    },
+		if ((event === undefined || event.target.classList.contains("next-button")) && imageIndex === images.length - 1) {
+			MySlider.resetAllImagePositions(images);
+			imageIndex = 0;
+			images[imageIndex].classList.add("active");
+		} else if ((event === undefined || event.target.classList.contains("next-button")) && imageIndex < images.length - 1) {
+			MySlider.slideImagesToNextPosition(images);
+		} else if (event.target.classList.contains("previous-button") && imageIndex > 0) {
+			MySlider.slideImagesToPreviousPosition(images);
+		}
+	};
 
-    resetAllImagePositions: function(imageArray) {
-      for (var i = 0; i < imageArray.length; i++) {
-        imageArray[i].classList.remove("inactive","active")
-      }
-    },
+	var resetAllImagePositions = function (imageArray) {
+		for (var i = 0; i < imageArray.length; i++) {
+			imageArray[i].classList.remove("inactive", "active")
+		}
+	};
 
-    slideImagesToPreviousPosition: function(imageArray) {
-      imageArray[imageIndex].classList.remove("inactive", "active");
-      imageIndex -= 1;
-      imageArray[imageIndex].classList.add("active");
-      imageArray[imageIndex].classList.remove("inactive");
-    },
+	var slideImagesToPreviousPosition = function (imageArray) {
+		imageArray[imageIndex].classList.remove("inactive", "active");
+		imageIndex -= 1;
+		imageArray[imageIndex].classList.add("active");
+		imageArray[imageIndex].classList.remove("inactive");
+	};
 
-    slideImagesToNextPosition: function(imageArray) {
-      imageArray[imageIndex].classList.add("inactive");
-      imageArray[imageIndex].classList.remove("active");
-      imageIndex += 1;
-      imageArray[imageIndex].classList.add("active");
-    },
+	var slideImagesToNextPosition = function (imageArray) {
+		imageArray[imageIndex].classList.add("inactive");
+		imageArray[imageIndex].classList.remove("active");
+		imageIndex += 1;
+		imageArray[imageIndex].classList.add("active");
+	};
 
-    startAutoPlayTimer: function(callbackFn, delay) {
-      timeoutId = setInterval(callbackFn, delay);
-    },
+	var startAutoPlayTimer = function (callbackFn, delay) {
+		timeoutId = setInterval(callbackFn, delay);
+	};
 
-    resetAutoPlayTimer: function() {
-      clearInterval(timeoutId)
-      MySlider.startAutoPlayTimer(MySlider.slide, MySlider.config.autoPlayDelay);
-    }
-  };
+	var resetAutoPlayTimer = function () {
+		clearInterval(timeoutId)
+		MySlider.startAutoPlayTimer(MySlider.slide, MySlider.config.autoPlayDelay);
+	};
 
+	var pulicMethods = {
+		config : config,
+		init : init,
+		updateConfig : updateConfig,
+		addWrapper : addWrapper,
+		addNavButton : addNavButton,
+		addButtonEventListeners : addButtonEventListeners,
+		slide : slide,
+		resetAllImagePositions : resetAllImagePositions,
+		slideImagesToPreviousPosition : slideImagesToPreviousPosition,
+		slideImagesToNextPosition : slideImagesToNextPosition,
+		startAutoPlayTimer : startAutoPlayTimer,
+		resetAutoPlayTimer : resetAutoPlayTimer
+	};
+
+	return pulicMethods;
 })();
